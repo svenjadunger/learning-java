3fc4976a5d1463736b3bcf03c494d9ada8c82ecd
Svenja Dunger <svenjadunger@web.de>
2024-06-05 09:58:01 +0200

    feat: complete lecture 1.4

--- HochschuleTest.java
+++ HochschuleTest.java
@@ -1,8 +1,11 @@
-// public class HochschuleTest {
-//     public static void main(String[] args) {
-//         Student tala = new Student("Tala", "Karoum");
-//         Student diana = new Student("Diana", "M");
-//         tala.setAlter(21);
-//         diana.setAlter(24);
-//     }
-// }
+public class HochschuleTest {
+    public static void main(String[] args) {
+        Student tala = new Student("Tala", "Karoum");
+        Student diana = new Student("Diana", "Mitrachow");
+        tala.setAlter(21);
+        diana.setAlter(24);
+
+        System.out.println(tala);
+        System.out.println(diana);
+    }
+}
