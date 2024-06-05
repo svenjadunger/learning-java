9a22a24c0c126c0c32511d83647bc9d13491939e
Svenja Dunger <svenjadunger@web.de>
2024-06-05 09:38:06 +0200

    feat: complete tasks 1-3

---
+++ UniApp.java
@@ -0,0 +1,37 @@
+
+
+public class UniApp {
+    public static void main(String[] args) {
+        // Student-
+        Student svenja = new Student("Svenja", "Dunger ", 12345, " Informatik");
+
+        svenja.kursBelegen("Prog1");
+        svenja.setAlter(17);
+
+        // Raum
+        Raum raum = new Raum("H101", 30);
+
+        // Dozent
+        Dozent martin = new Dozent("Martin", "GSE");
+        martin.setFachbereich("Prog-Tutorium");
+
+        
+        System.out.println(svenja);
+        System.out.println(svenja.getAllData());
+
+        
+        // for(int i = 0; i < svenja.belegteKurse.length; i++){
+        //     if (svenja.belegteKurse[i] != null) {
+        //         System.out.println(svenja.belegteKurse[i]);
+        //     }
+        // }
+
+        
+        System.out.println("Dozent Vorname: " + martin.getVorname());
+        // System.out.println("Neuer Fachbereich: " + martin.getFachbereich());
+
+        
+        System.out.println("Raum: " + raum.getRaumNummer());
+    }
+}
+
