8653b011eb4584aa5732bd0c0a5100978ffc99c0
Svenja Dunger <svenjadunger@web.de>
2024-06-05 10:10:11 +0200

    feat: complete task 1, 2 arrays

--- HochschuleTest.java
+++ HochschuleTest.java
@@ -7,5 +7,22 @@ public class HochschuleTest {
 
         System.out.println(tala);
         System.out.println(diana);
+
+        //Array von Student-Objekten
+        Student[] studenten = new Student[6];
+
+        studenten[0] = new Student("Svenja", "Dunger", 12345, "Wirtschaftsinformatik");
+        studenten[1] = new Student("Tala", "Karoum", 12346, "Wirtschaftsinformatik");
+        studenten[2] = new Student("Diana", "M", 12347, "Wirtschaftsinformatik");
+        studenten[3] = new Student("John", "Doe", 12348, "Elektrotechnik");
+        studenten[4] = new Student("Jane", "Smith", 12349, "Medizin");
+        studenten[5] = new Student("Max", "Mustermann", 12350, "BWL");
+
+        //alle student-objekte ausgeben
+
+        for (Student student : studenten){
+            System.out.println(student);
+        }
+
     }
 }
